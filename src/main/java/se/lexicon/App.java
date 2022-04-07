7c08d70712d08953932ed64900065293acb158d7
Leo Yoosefi <101973222+leoyoosefi@users.noreply.github.com>
2022-04-07 09:56:57 +0200

    Array workshop progress

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,32 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(NameRepository.getSize());
+        String[] names = {"Erik Svensson", "Mehrdad Javan", "Erik Alfredsson"};
+        NameRepository.setNames(names);
+        System.out.println(NameRepository.getSize());
+        //NameRepository.clear();
+        System.out.println(NameRepository.getSize());
+        System.out.println(Arrays.toString(NameRepository.findAll()));
+        System.out.println(NameRepository.find("Mehrdad Javan"));
+        System.out.println(NameRepository.find("Test Test1"));
+        System.out.println(NameRepository.add("Simon Elbrink"));
+        System.out.println(Arrays.toString(NameRepository.findAll()));
+        boolean newName = NameRepository.add("Mehrdad Javan");
+        if (newName){
+            System.out.println("Name is Added to Array");
+        } else {
+            System.out.println("Name is Duplicate!");
+        }
+
+        System.out.println(Arrays.toString(NameRepository.findAll()));
+
+        System.out.println(Arrays.toString(NameRepository.findByFirstName("Erik")));
+
+
     }
 }
